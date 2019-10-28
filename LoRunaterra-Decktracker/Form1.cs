b15d8fe871f0844555f70a0fe77ea1b808c8e38f
Victor <vicentdondona@gmail.com>
2019-10-28 11:54:40 +0100

    Añadido DLL y decodificacion mazos
    Se ha añadido el DLL proporcionado por Riot para deocidificar los codigos de los mazos y se ha añadido un pequeño ejemplo de uso en Form1

--- LoRunaterra-Decktracker/Form1.cs
+++ LoRunaterra-Decktracker/Form1.cs
@@ -4,17 +4,30 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LoRDeckCodes;
+using static LoRDeckCodes.LoRDeckEncoder;
+
+
 
 namespace LoRunaterra_Decktracker
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            List<CardCodeAndCount> aux = new List<CardCodeAndCount>();
+            aux = GetDeckFromCode("CEBAIAIFB4WDANQIAEAQGDAUDAQSIJZUAIAQCBIFAEAQCBAA");
+            foreach (var x in aux)
+            {
+                Console.WriteLine(x.CardCode);
+                Console.WriteLine(x.Count);
+            }
 
             //Inicializamos el formulario de status
             IniciarStatus();
